54ac5aa643f20892fd91b20dd8dfced7a7763ace
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-17 19:44:09 +0530

    added change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,19 +11,13 @@ const Header = ({ showCartHandler }) => {
   return (
     <Navbar expand="lg" bg="dark" data-bs-theme={"dark"}>
       <Container>
-        <Navbar.Brand href="#home">The Generics</Navbar.Brand>
+        <Navbar.Brand onClick={() => navigate("/")}>The Generics</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
-            <Nav.Link onClick={() => navigate("/")} href="#">
-              Home
-            </Nav.Link>
-            <Nav.Link onClick={() => navigate("/store")} href="#">
-              Store
-            </Nav.Link>
-            <Nav.Link onClick={() => navigate("/about")} href="#">
-              About
-            </Nav.Link>
+            <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
+            <Nav.Link onClick={() => navigate("/store")}>Store</Nav.Link>
+            <Nav.Link onClick={() => navigate("/about")}>About</Nav.Link>
           </Nav>
           <Nav className="justify-content-end">
             <HeaderCartButton showCartHandler={showCartHandler} />
